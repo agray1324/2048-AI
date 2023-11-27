ff2c8e82d0f5dc066a161fc7bccf9ae463f7a83b
agray1324 <85634946+agray1324@users.noreply.github.com>
2023-11-27 11:41:11 -0500

    timed search

--- minimax.py
+++ minimax.py
@@ -1,7 +1,11 @@
 from ai import *
 import sys
+import time
 
-def Player(board, depth, a, b):
+def Player(board, depth, a, b, end = False):
+    if end:
+        if time.time() > end:
+            return -1, 0
     scores, states = getScoresAndNextStates(board)
     moves = getNextLegalMoves(board)
     directions = ['n', 'e', 's', 'w']
@@ -14,13 +18,19 @@ def Player(board, depth, a, b):
         bestAction = -1
         for move in moves:
             state = states[directions.index(move)]
-            newScore, newAction = boardMove(state, depth, a, b)
+            newScore, newAction = boardMove(state, depth, a, b, end)
             if(newScore > s):
                 s = newScore
                 bestAction = move
                 a = max(a, s)
+                if end:
+                    if time.time() > end:
+                        return -1, 0
                 if(s > b):
                     return s, bestAction
+        if end:
+            if time.time() > end:
+                return -1, 0
         return s, bestAction
         """if state == board:
             score = 0
@@ -36,22 +46,28 @@ def Player(board, depth, a, b):
         rec_scores.append(score)
     return moves[rec_scores.index(max(rec_scores))], max(rec_scores)"""
     
-def boardMove(state, depth, a, b):
+def boardMove(state, depth, a, b, end):
     states2, states4 = getAllInsertStates(state)
     states = states2 + states4
     s = sys.maxsize
     for state in states:
-        newScore, newAction = Player(state, depth-1, a, b)
+        newScore, newAction = Player(state, depth-1, a, b, end)
         if(newScore < s):
             s = newScore
             b = min(b, s)
+            if end:
+                if time.time() > end:
+                    return -1, 0
             if (s < a):
                 return s, ""
+    if end:
+        if time.time() > end:
+            return -1, 0
     return s, ""
 
-def getMove(depth = 5):
+def getMove(depth = 5, end = False):
     global board
-    score, move = Player(board, depth, -1*sys.maxsize, sys.maxsize)
+    score, move = Player(board, depth, -1*sys.maxsize, sys.maxsize, end)
     print(move, score)
     return move, score
 
@@ -77,6 +93,25 @@ def depthAnalysis(minimum = 1, maximum = 6):
         print("max: ", max(maxes))
         print("average: ",sum(maxes)/len(maxes))
 
+def timedSearch(seconds):
+    global board
+    board = initialize_board()
+    while not is_game_over(board):
+        end = time.time() + seconds
+        best_move, best_score = -1, 0
+        depth = 1
+        while(time.time() < end):
+            newMove, newScore = getMove(depth, end)
+            if(newScore >= best_score):
+                best_score = newScore
+                best_move = newMove
+            depth+=1
+        board = move(board, best_move)
+        print(score(board))
+        showBoard(board)
+    return max_function(board)
+            
+
 def singleTest(depth):
     global board
     board = initialize_board()
@@ -88,7 +123,8 @@ def singleTest(depth):
     return max_function(board)
 
 iterations = 100
-results = {2:0, 3:0,4:0,5:0,6:0,7:0, 8:0, 9:0, 10:0, 11:0, 12:0, 13:0, 14:0}
+results = {2:0, 3:0,4:0,5:0,6:0,7:0, 8:0, 9:0, 10:0, 11:0, 12:0, 13:0, 14:0, 15:0, 16:0}
 for i in range(iterations):
-    results[singleTest(3)]+=1
+    results[timedSearch(1)]+=1
     print(results)
+
